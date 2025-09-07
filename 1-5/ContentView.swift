563a5645ceeded681dc4f5a59b60867248d026b1
thunkit <taran@patibanda.net>
2025-09-07 17:51:27 -0400

    Initial Commit

---
+++ 1-5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  1-5
+//
+//  Created by Taran Patibanda on 9/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 1-5/__5App.swift
@@ -0,0 +1,17 @@
+//
+//  __5App.swift
+//  1-5
+//
+//  Created by Taran Patibanda on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct __5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
